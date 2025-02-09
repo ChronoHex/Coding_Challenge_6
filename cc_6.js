c3d906a21877a09c3a5e7cbb6e92f4fe9c30df8b
Eugene Rodriguez <er11@usf.edu>
2025-02-09 17:43:43 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -15,3 +15,21 @@ const calculateSalesTax = function(amount, taxRate) {
 
 console.log(calculateSalesTax(100, 0.07)); // Sales Tax: $7
 console.log(calculateSalesTax(500, 0.1)); // Sales Tax: $50
+
+// Task 3: Arrow Function
+const calculateBonus = (salary, performanceRating) => {
+    let bonus;
+    if (performanceRating === "Average") {
+        bonus = salary * 0.05;
+    }
+    else if (performanceRating === "Good") {
+        bonus = salary * 0.1;
+    }
+    else if (performanceRating === "Excellent") {
+        bonus = salary * 0.2;
+    }
+    return `Bonus: $${bonus}`;
+}; // function for calculating bonuses
+
+console.log(calculateBonus(5000, "Excellent")); // Bonus: $1000
+console.log(calculateBonus(7000, "Good")); // Bonus: $700
